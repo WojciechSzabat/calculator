d15aa702f21842421f32f6607e86cb65732dc42a
wojtek <wojtaks10@gmail.com>
2021-03-05 22:30:34 +0100

    buttons

---
+++ main.js
@@ -0,0 +1,10 @@
+const valueToDisplay = document.querySelector("#text1");
+console.log(valueToDisplay.textContent)
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(button => {
+        button.addEventListener('click', (e)=>{
+            
+            
+        })
+})
